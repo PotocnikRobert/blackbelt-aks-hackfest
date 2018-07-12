1b246e21ec0714df2ef58ddc203dfa3a9abb459b
RobertPotocnik <robert.potocnik@live.com>
2018-07-12 16:45:01 -0700

    Update  footer

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Azure Global Blackbelt Team1111
+        Azure Global Blackbelt T
       </div>
       <div class="col-lg-6">
       </div>
